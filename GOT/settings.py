77f992d31970643ba1b74c3300439c38504faeb4
DurbhAditya99 <adityadurbha@gmail.com>
2021-06-30 15:50:40 +0530

    second

--- GOT/settings.py
+++ GOT/settings.py
@@ -31,7 +31,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
