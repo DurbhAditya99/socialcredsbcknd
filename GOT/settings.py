68d14703a4d463043f0ed3ec6874b6729c1568fa
DurbhAditya99 <adityadurbha@gmail.com>
2021-06-30 16:14:10 +0530

    third

--- GOT/settings.py
+++ GOT/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','socialcreds.herokuapp']
 
 
 # Application definition
